0195952b2d8c4f23953238cad714299ae0fccff4
Danish Zaidan <95460214+Dzkamil@users.noreply.github.com>
2024-05-20 17:11:15 +0700

    index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Grapara Home</title>
+</head>
+<body>
+    <h2>Welcome to Grapara</h2>
+    <ul>
+        <li><a href="get_queue_number.php">Get Queue Number</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</body>
+</html>
